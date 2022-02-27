868eefc52d0fb1bc3091cd7beb5255ced200c254
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-02-27 11:38:15 -0500

    added useEffect and function checkIfWalletConnected

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
 
+declare const window: any
+
 const App: React.FC = () => {
+
+  const checkIfWalletIsConnected = async (): Promise<void> => {
+    try {
+      const {solana}  = window;
+      if (solana && solana.isPhantom) {
+        console.log('Phantom wallet found!')
+      } else {
+        alert('Get a Phantom Wallet')
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, [])
+
+
   return (
     <div>Solana</div>
   )
